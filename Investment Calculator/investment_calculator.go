3cd5a1dd2938442b6ccdfec60ccfa1e089725f4e
Gerard Causse <grod.causse@gmail.com>
2025-06-20 15:49:51 -0400

    Complete Understanding Functions lesson

--- Investment Calculator/investment_calculator.go
+++ Investment Calculator/investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount, expectedReturnRate, years float64
 
 	fmt.Print("Investment Amount: ")
@@ -18,8 +19,7 @@ func main() {
 	fmt.Print("Years Invested: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+(expectedReturnRate/100), years)
-	realFutureValue := futureValue / math.Pow(1+(inflationRate/100), years)
+	futureValue, realFutureValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.2f\n", realFutureValue)
@@ -28,4 +28,16 @@ func main() {
 	// fmt.Println("Future Value (adjusted for inflation): ", realFutureValue)
 	// fmt.Printf("Future Value (adjusted for inflation): %.2f\n", realFutureValue)
 	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string, value float64) {
+	fmt.Print(text)
+	fmt.Scan(&value)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+(expectedReturnRate/100), years)
+	rfv := fv / math.Pow(1+(inflationRate/100), years)
+
+	return fv, rfv
 }
--- profit.go
+++ profit.go
@@ -5,21 +5,30 @@ import "fmt"
 func main() {
 	var revenue, expenses, taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	revenue = getUserInput("Revenue: ")
+	expenses = getUserInput("Expenses: ")
+	taxRate = getUserInput("Tax Rate: ")
 
 	// Earnings Before Tax
+	ebt, profit, ratio := calculateProfits(revenue, expenses, taxRate)
+
+	fmt.Printf("Earnings before tax: %.2f\n", ebt)
+	fmt.Printf("Profit: %.2f\n", profit)
+	fmt.Printf("Earnings to profit ratio: %.2f\n", ratio)
+}
+
+func getUserInput(text string) float64 {
+	fmt.Print(text)
+	var input float64
+	fmt.Scan(&input)
+
+	return input
+}
+
+func calculateProfits(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 
-	fmt.Println("Earnings before tax: ", ebt)
-	fmt.Println("Profit: ", profit)
-	fmt.Println("Earnings to profit ratio: ", ratio)
+	return ebt, profit, ratio
 }
