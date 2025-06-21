9f05c9abdcda92c3bacd828169d9262dcef9b375
Gerard Causse <grod.causse@gmail.com>
2025-06-20 23:53:57 -0400

    Complete conditional lessons

--- Bank/bank.go
+++ Bank/bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance float64 = 1_000
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
@@ -14,5 +16,36 @@ func main() {
 	fmt.Print("Enter choice: ")
 	fmt.Scan(&choice)
 
-	fmt.Println("Your choice: ", choice)
+	if choice == 1 {
+		var message string = fmt.Sprintf("Your balance is %.2f", accountBalance)
+		fmt.Println(message)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		if depositAmount >= 0 {
+			accountBalance += depositAmount
+		} else {
+			fmt.Println("Invalid amount")
+		}
+	} else if choice == 3 {
+		fmt.Print("Withdraw amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+		if withdrawAmount < 0 {
+			fmt.Println("Invalid amount")
+			return
+		}
+		if withdrawAmount <= accountBalance {
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated: ", accountBalance)
+		} else {
+			fmt.Println("Insufficient funds")
+		}
+	} else if choice == 4 {
+		fmt.Println("Goodbye!")
+	} else {
+		fmt.Println("Invalid choice")
+	}
+
 }
