04e2dae57632bf764fe546f9d70637c5927e22ec
Gerard Causse <grod.causse@gmail.com>
2025-06-22 15:59:29 -0400

    Complete Creating Custom Other Custom Types & Adding Methods lesson

--- Structs/structs.go
+++ Structs/structs.go
@@ -6,7 +6,16 @@ import (
 	usr "example.com/structs/user"
 )
 
+type str string
+
+func (text str) log() {
+	fmt.Println(text)
+}
+
 func main() {
+	var name str = "John"
+	name.log()
+
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
