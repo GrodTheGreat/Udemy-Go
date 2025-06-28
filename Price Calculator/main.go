58546f43b488c4bb316178fe0fe3fe19a2580f4f
Gerard Causse <grod.causse@gmail.com>
2025-06-27 22:12:04 -0400

    Reformat

--- Price Calculator/main.go
+++ Price Calculator/main.go
@@ -11,7 +11,7 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100)) //Ignore this error, this is here as an example of how fm and cm are "interchangable"
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		// cm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		err := priceJob.Process()
@@ -19,7 +19,5 @@ func main() {
 			fmt.Println("could not process job")
 			fmt.Println(err)
 		}
-
 	}
-
 }
