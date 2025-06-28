525ba5864eb837d176bad1c551cb693b488501f3
Gerard Causse <grod.causse@gmail.com>
2025-06-27 22:00:32 -0400

    Complete interfaces to the Rescue lesson

--- Price Calculator/main.go
+++ Price Calculator/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"example.com/price-calculator/cmdmanager"
 	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
@@ -12,9 +11,9 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cm := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(cm, taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100)) //Ignore this error, this is here as an example of how fm and cm are "interchangable"
+		// cm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob.Process()
 
 	}
--- Price Calculator/prices/prices.go
+++ Price Calculator/prices/prices.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"example.com/price-calculator/conversion"
-	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	IOManager         *filemanager.FileManager `json:"-"`
-	TaxRate           float64                  `json:"taxRate"`
-	InputPrices       []float64                `json:"inputPrices"`
-	TaxIncludedPrices map[string]string        `json:"taxIncludedPrices"`
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"taxRate"`
+	InputPrices       []float64           `json:"inputPrices"`
+	TaxIncludedPrices map[string]string   `json:"taxIncludedPrices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -44,9 +44,9 @@ func (job *TaxIncludedPriceJob) Process() {
 	job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(fm *filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(io iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		IOManager:   fm,
+		IOManager:   io,
 		TaxRate:     taxRate,
 		InputPrices: []float64{10, 20, 30},
 	}
