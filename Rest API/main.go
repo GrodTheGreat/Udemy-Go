cc1995dd487b5e06bffe6183d4e5825ed710e860
Gerard Causse <grod.causse@gmail.com>
2025-06-29 11:39:08 -0400

    Complete Setting Up a First Route & Handling a First Request lesson

--- Rest API/main.go
+++ Rest API/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
 func main() {
-	gin.Default()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	log.Fatal(server.Run(":8080"))
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
 }
