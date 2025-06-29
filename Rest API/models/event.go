65435ac6485a84ca30b134c6aa064df45f1d7e0e
Gerard Causse <grod.causse@gmail.com>
2025-06-29 12:53:14 -0400

    Complete Storing Data in the Database (INSERT) lesson

--- Rest API/models/event.go
+++ Rest API/models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"rest/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -11,10 +14,30 @@ type Event struct {
 	UserID      int
 }
 
-var events []Event = []Event{}
+func (event *Event) Save() error {
+	query := `
+INSERT INTO events(name, description, location, datetime, user_id)
+VALUES(?, ?, ?, ?, ?)
+`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-func (event *Event) Save() {
-	events = append(events, *event)
+	result, err := stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	event.ID = id
+
+	return nil
 }
 
 func GetAllEvents() []Event {
