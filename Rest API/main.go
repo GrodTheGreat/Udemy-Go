2227118cb8610b5d9a0b850b611d3c6f811b3b84
Gerard Causse <grod.causse@gmail.com>
2025-06-29 14:22:13 -0400

    Complete Getting Events From Database (SELECT) lesson

--- Rest API/main.go
+++ Rest API/main.go
@@ -19,7 +19,12 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch events. Try again later"})
+		log.Fatal(err)
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -27,13 +32,17 @@ func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		log.Fatal(err)
 		return
 	}
 
 	event.ID = 1
 	event.UserID = 1
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to save event. Try again later"})
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
--- Rest API/models/event.go
+++ Rest API/models/event.go
@@ -40,8 +40,32 @@ VALUES(?, ?, ?, ?, ?)
 	return nil
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `
+SELECT id, name, description, location, datetime, user_id
+FROM events
+`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	var rawDateTime string
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &rawDateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		event.DateTime, err = time.Parse(time.RFC3339, rawDateTime)
+
+		events = append(events, event)
+	}
+
+	return events, nil
 }
 func New() *Event {
 	return &Event{}
