0e65f9942b1cd87c64ea6d6894a693c2a4972da3
Gerard Causse <grod.causse@gmail.com>
2025-06-29 12:09:13 -0400

    Complete Testing Requests & Fixing the POST Request Handler lesson

---
+++ Rest API/models/event.go
@@ -0,0 +1,25 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events []Event = []Event{}
+
+func (event *Event) Save() {
+	events = append(events, *event)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
+func New() *Event {
+	return &Event{}
+}
