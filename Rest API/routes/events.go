c58ffe74dd9a875256fcba0cb73a5cb3ea82c11a
Gerard Causse <grod.causse@gmail.com>
2025-06-30 00:19:29 -0400

    Complete Adding Route Protection lesson

--- Rest API/routes/events.go
+++ Rest API/routes/events.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"rest/models"
+	"rest/utils"
 	"strconv"
 )
 
@@ -19,15 +20,25 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
 		log.Println(err)
 		return
 	}
 
-	event.ID = 1
 	event.UserID = 1
 	err = event.Save()
 	if err != nil {
